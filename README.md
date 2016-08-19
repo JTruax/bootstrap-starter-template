1313ad92185c9d598e0380441a2b198ac20cce8d
JTruax <justin.truax87@gmail.com>
2016-08-19 09:10:49 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+# [New Bootstrap Template](https://github.com/JTruax/bootstrap-starter-template)
+
+Create a new HTML file with a Bootstrap starter template using file menu or Ctrl-Alt-B or side bar double-click.
+
+![Screenshot](http://justintruax.com/wp-content/uploads/2016/08/brackets-bootstrap-template.gif)
+
+## How to Install
+---
+
+#### Git Clone
+
+1. Under main menu select **Help > Show Extensions Folder**
+2. Git clone this repository inside the folder user.
+
+#### Extension Manager
+
+1. Under main menu select **File > Extension Manager...**
+2. Search for "Boostrap Starter Template
+3. Click "Install"
+
+## How to use
+---
+
+1. Use "New Bootstrap Template" on file menu, or
+2. Double-click on side bar, or
+3. Press Ctrl-alt-B;
---
+++ main.js
@@ -0,0 +1,54 @@
+define(function (require, exports, module) {
+	"use strict";
+
+	var docIndex = 1,
+	    commandId = "new-bootstrap-page",
+	    menuID = "jt.menuID",
+	    menuLabel = "New Bootstrap Template",
+		DocumentManager = brackets.getModule("document/DocumentManager"),
+		Commands = brackets.getModule("command/Commands"),
+		CommandManager = brackets.getModule("command/CommandManager"),
+		KeyBindingManager = brackets.getModule("command/KeyBindingManager"),
+		EditorManager = brackets.getModule("editor/EditorManager"),
+		MainViewManager = brackets.getModule("view/MainViewManager"),
+		Menus = brackets.getModule("command/Menus"),
+		bootstrapTemplate = require("text!template/start.html"),
+		sidebar = $("#sidebar"),
+		toolbar = $("#main-toolbar"),
+		menu;
+
+
+	function templateHandle(templateContent) {
+		try {
+			var activeEditor = EditorManager.getActiveEditor();
+			activeEditor.document.replaceRange(templateContent, activeEditor.getCursorPos());
+		} catch (err) {}
+	}
+
+	function newFileHandle() {
+		var defaultExtension = ".html",
+		    doc = DocumentManager.createUntitledDocument(docIndex++, defaultExtension);
+
+		MainViewManager._edit(MainViewManager.ACTIVE_PANE, doc);
+		templateHandle(bootstrapTemplate);
+		return new $.Deferred().resolve(doc).promise();
+	}
+
+	sidebar.on('dblclick', 'div', function (e) {
+		if (e.target === this) {
+			newFileHandle();
+		}
+	});
+
+	toolbar.on('dblclick', function (e) {
+		if (e.target === this) {
+			newFileHandle();
+		}
+	});
+
+	CommandManager.register(menuLabel, menuID, newFileHandle);
+	menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
+	menu.addMenuItem(menuID, undefined, Menus.AFTER, Commands.FILE_NEW_UNTITLED);
+    KeyBindingManager.addBinding(menuID, "Ctrl-Alt-B", "mac");
+	KeyBindingManager.addBinding(menuID, "Ctrl-Alt-B", "win");
+});
